b6b6c7593cdd42ac00aec72d3b6f7486269c8132
iApurvJha <apurvjha302@gmail.com>
2023-05-03 23:04:33 +0530

    completed-banana-talk

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-var textarea=document.querySelector("#textarea")
+var textarea = document.querySelector("#textarea")
 var translate = document.querySelector("#btn-translate")
 var output = document.querySelector("#output")
 
+function UrlGen() {
 
+    return "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + textarea.value
 
-translate.addEventListener("click", function Eventhandler(){
-    output.innerText = textarea.value
-    console.log("clicked")
+}
+
+function errorHandler(error) {
+    console.log("error", error)
+    alert("some error occured try again after some time")
+}
+
+translate.addEventListener("click", function Eventhandler() {
+    var url = UrlGen()
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            var translatedtext = json.contents.translated;
+            output.innerText = translatedtext
+        })
+        .catch(errorHandler)
+
 })
